cbb614ec6f8a0bbb62f41254703b70f715b447dc
LostB053 <72547660+lostb053@users.noreply.github.com>
2021-07-29 16:13:25 +0530

    -_-

--- lazyleech/utils/upload_worker.py
+++ lazyleech/utils/upload_worker.py
@@ -24,6 +24,8 @@ import asyncio
 import zipfile
 import tempfile
 import traceback
+import subprocess
+from uuid import uuid4
 from collections import defaultdict
 from natsort import natsorted
 from pyrogram.parser import html as pyrogram_html
